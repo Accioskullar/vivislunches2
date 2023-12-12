064c97f996f410d15f992d556fe05f6d2a120a27
Gerson Rodriguez <101367282+Accioskullar@users.noreply.github.com>
2023-12-12 19:54:04 -0400

    pleaaaaaaaaaaaaaaaase

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -3,6 +3,31 @@ import { getAuthSession } from "@/utils/auth";
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
+const allowCors = (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any; }) => async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new(): any; end: { (): void; new(): any; }; }; }) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req: any, res: { end: (arg0: string) => void; }) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
+
 // FETCH ALL ORDERS
 export const GET = async (req: NextRequest) => {
   // Fetching the user session using the getAuthSession utility function
